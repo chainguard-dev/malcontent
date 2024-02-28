45350bf9d75cce91f336dc21b2df7653ee51190e
Thomas Stromberg <t+github@chainguard.dev>
2024-02-27 21:58:13 -0500

    Add support recursive directory traversal

--- pkg/bincapz/scan.go
+++ pkg/bincapz/scan.go
@@ -3,6 +3,8 @@ package bincapz
 import (
 	"fmt"
 	"io/fs"
+	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/hillu/go-yara/v4"
@@ -15,6 +17,24 @@ type Config struct {
 	IgnoreTags      []string
 	MinLevel        int
 	ThirdPartyRules bool
+}
+
+// return a list of files within a path
+func findFilesRecursively(root string) ([]string, error) {
+	files := []string{}
+
+	err := filepath.Walk(root,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if info.IsDir() {
+				return nil
+			}
+			files = append(files, path)
+			return nil
+		})
+	return files, err
 }
 
 func Scan(c Config) (*Report, error) {
@@ -33,16 +53,23 @@ func Scan(c Config) (*Report, error) {
 
 	klog.V(1).Infof("%d rules loaded", len(yrs.GetRules()))
 
-	for _, p := range c.ScanPaths {
-		var mrs yara.MatchRules
-		// klog.Infof("scanning: %s", p)
-		if err := yrs.ScanFile(p, 0, 0, &mrs); err != nil {
-			r.Files[p] = FileReport{Error: fmt.Sprintf("scanfile: %v", err)}
-			continue
+	for _, sp := range c.ScanPaths {
+		rp, err := findFilesRecursively(sp)
+		if err != nil {
+			return nil, fmt.Errorf("find files: %w", err)
 		}
+		// TODO: support zip files and such
+		for _, p := range rp {
+			var mrs yara.MatchRules
+			// klog.Infof("scanning: %s", p)
+			if err := yrs.ScanFile(p, 0, 0, &mrs); err != nil {
+				r.Files[p] = FileReport{Error: fmt.Sprintf("scanfile: %v", err)}
+				continue
+			}
 
-		fr := fileReport(mrs, c.IgnoreTags, c.MinLevel)
-		r.Files[p] = fr
+			fr := fileReport(mrs, c.IgnoreTags, c.MinLevel)
+			r.Files[p] = fr
+		}
 	}
 
 	return r, nil
