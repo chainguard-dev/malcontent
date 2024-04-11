797d823519ab60741f399fa873cf13028c9baf7d
Thomas Stromberg <t+github@chainguard.dev>
2024-04-10 22:24:25 -0400

    Revert errant testdata -> samples change

--- pkg/action/programkind_test.go
+++ pkg/action/programkind_test.go
@@ -39,7 +39,7 @@ func TestProgramStringMatch(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.filename, func(t *testing.T) {
 			ctx := slogtest.TestContextWithLogger(t)
-			got := programKind(ctx, fmt.Sprintf("samples/%s", tt.filename))
+			got := programKind(ctx, fmt.Sprintf("testdata/%s", tt.filename))
 			if got != tt.want {
 				t.Errorf("programKind() = %v, want %v", got, tt.want)
 			}
