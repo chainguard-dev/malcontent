351c2be88ebd3824ce979b6a2dcdbf9a00df7cab
Thomas Strömberg <t+github@chainguard.dev>
2024-04-11 08:26:06 -0400

    Hide log messages from stderr by default (#109)

--- bincapz.go
+++ bincapz.go
@@ -32,7 +32,7 @@ func main() {
 	includeDataFilesFlag := flag.Bool("data-files", false, "include files that are detected to as non-program (binary or source) files")
 	diffFlag := flag.Bool("diff", false, "show capability drift between two files")
 	allFlag := flag.Bool("all", false, "Ignore nothing, show all")
-	logSource := flag.Bool("log-source", false, "Include source code location in log messages")
+	verboseFlag := flag.Bool("verbose", false, "emit verbose logging messages to stderr")
 
 	flag.Parse()
 	args := flag.Args()
@@ -42,8 +42,16 @@ func main() {
 		os.Exit(1)
 	}
 
-	log := clog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{AddSource: *logSource}))
+	logLevel := new(slog.LevelVar)
+	logLevel.Set(slog.LevelError)
+	logOpts := &slog.HandlerOptions{Level: logLevel}
 
+	if *verboseFlag {
+		logOpts.AddSource = true
+		logLevel.Set(slog.LevelDebug)
+	}
+
+	log := clog.New(slog.NewTextHandler(os.Stderr, logOpts))
 	ctx := clog.WithLogger(context.Background(), log)
 	clog.FromContext(ctx).Info("bincapz starting")
 
