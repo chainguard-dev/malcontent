4b2d39a38c998ff19847c50572e074f6baea2844
Thomas Stromberg <t+github@chainguard.dev>
2024-05-07 13:47:36 -0400

    rename keywords to threat_hunting_keywords

--- third_party/yara/update.sh
+++ third_party/yara/update.sh
@@ -19,7 +19,7 @@ latest_github_release() {
 
 # fixup_rules fixes rules up, including lightly obfuscating them to avoid XProtect from matching bincapz
 function fixup_rules() {
-	perl -p -i -e 's#"/Library/Application Support/Google/Chrome/Default/History"#/\\/Library\\/Application Support\\/Google\\/Chrome\\/Default\\/History\/#' "$@"
+	perl -p -i -e 's#"/Library/Application Support\/Google/Chrome/Default/History"#/\\/Library\\/Application Support\\/Google\\/Chrome\\/Default\\/History\/#' "$@"
 	perl -p -i -e 's#\/([a-z]{31})([a-z])\/#\/$1\[$2\]\/#;' "$@"
 	# trailing spaces
 	perl -p -i -e 's/ +$//;' "$@"
@@ -27,41 +27,45 @@ function fixup_rules() {
 
 # update_dep updates a dependency to the latest release
 function update_dep() {
-    local kind=$1
-    local tmpdir=$(mktemp -d)
+	local kind=$1
+	local tmpdir=$(mktemp -d)
 	local rel="unknown"
 
-    case $kind in
-        YARAForge)
-			rel=$(latest_github_release YARAHQ/yara-forge)
-            curl -L -o "${tmpdir}/yaraforge.zip" "https://github.com/YARAHQ/yara-forge/releases/download/${rel}/yara-forge-rules-full.zip"
-            unzip -o -j "${tmpdir}/yaraforge.zip" packages/full/yara-rules-full.yar -d "${kind}"
-        ;;
-        huntress)
-			git clone https://github.com/huntresslabs/threat-intel.git "${tmpdir}"
-			pushd "${tmpdir}" || exit 1
-			rel="$(git rev-parse head)"
-			popd || exit 1
-			find "${tmpdir}" \( -name "*.yar*" -o -name "*LICENSE*" \) -print -exec cp {} "${kind}" \;
+	case $kind in
+	YARAForge)
+		rel=$(latest_github_release YARAHQ/yara-forge)
+		curl -L -o "${tmpdir}/yaraforge.zip" "https://github.com/YARAHQ/yara-forge/releases/download/${rel}/yara-forge-rules-full.zip"
+		unzip -o -j "${tmpdir}/yaraforge.zip" packages/full/yara-rules-full.yar -d "${kind}"
 		;;
-		keywords)
-			rel=$(latest_github_release mthcht/ThreatHunting-Keywords-yara-rules)
-            curl -L -o "${tmpdir}/keywords.zip" "https://github.com/mthcht/ThreatHunting-Keywords-yara-rules/archive/refs/tags/${rel}.zip"
-            unzip -o -j "${tmpdir}/keywords.zip" ThreatHunting-Keywords-yara-rules-1.0.1/yara_rules/all.yara -d "${kind}"
+	huntress)
+		git clone https://github.com/huntresslabs/threat-intel.git "${tmpdir}"
+		pushd "${tmpdir}" || exit 1
+		rel="$(git rev-parse head)"
+		popd || exit 1
+		find "${tmpdir}" \( -name "*.yar*" -o -name "*LICENSE*" \) -print -exec cp {} "${kind}" \;
 		;;
-    esac
-	
+	threat_hunting_keywords)
+		rel=$(latest_github_release mthcht/ThreatHunting-Keywords-yara-rules)
+		curl -L -o "${tmpdir}/keywords.zip" "https://github.com/mthcht/ThreatHunting-Keywords-yara-rules/archive/refs/tags/${rel}.zip"
+		unzip -o -j "${tmpdir}/keywords.zip" ThreatHunting-Keywords-yara-rules-1.0.1/yara_rules/all.yara -d "${kind}"
+		;;
+	*)
+		echo "unknown kind: ${kind}"
+		exit 2
+		;;
+	esac
+
 	fixup_rules ${kind}/*.yar* # nolint
-	echo "${rel}" > "${kind}/RELEASE"
+	echo "${rel}" >"${kind}/RELEASE"
 	echo "updated ${kind} to ${rel}"
 }
 
 if [[ "$1" != "" ]]; then
-  update_dep "$1"
+	update_dep "$1"
 else
-  for dir in *; do
-    if [[ -f "${dir}/RELEASE" ]]; then
-	  update_dep "${dir}"
-	fi
-   done
+	for dir in *; do
+		if [[ -f "${dir}/RELEASE" ]]; then
+			update_dep "${dir}"
+		fi
+	done
 fi
