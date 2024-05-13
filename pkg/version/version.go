1ab61fcdbe9dbfdfed1f0ad893891ef2c30d6290
Evan Gibler <keybase@egibs.xyz>
2024-05-13 14:44:57 -0500

    Add version support (#210)
    * Add version support
    
    * Appease the linter
    
    * make fix
    
    * Match GoReleaser config
    
    * Store version in version package
    
    * Remove VERSION file
    
    * Fix indentation
    
    * Capitalize flag usage
    
    ---------
    
    Signed-off-by: Evan Gibler <keybase@egibs.xyz>

---
+++ pkg/version/version.go
@@ -0,0 +1,41 @@
+package version
+
+import (
+	"fmt"
+	"runtime/debug"
+)
+
+const (
+	ID string = "0.11.0"
+)
+
+// Check if the build info contains a version.
+func getBinaryVersion() (string, error) {
+	buildInfo, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "", fmt.Errorf("failed to read build info")
+	}
+
+	for _, setting := range buildInfo.Settings {
+		if setting.Key == "main.BuildVersion" {
+			return setting.Value, nil
+		}
+	}
+
+	return "", nil
+}
+
+func Version() (string, error) {
+	var v string
+	var err error
+	// Check for the version in the binary first
+	if v, err = getBinaryVersion(); err != nil {
+		return "bincapz unknown version", err
+	}
+	// If present, return that value
+	// Otherwise, fall back to the contents of the VERSION const
+	if v != "" {
+		return fmt.Sprintf("bincapz %s", v), nil
+	}
+	return fmt.Sprintf("bincapz %s", ID), nil
+}
