bbc4d4d97e993a2ba3e70ddb5020f143ab7b3df5
egibs <20933572+egibs@users.noreply.github.com>
2024-09-10 07:33:46 -0500

    Overhaul CLI functionality with urfave/cli
    Signed-off-by: egibs <20933572+egibs@users.noreply.github.com>

--- bincapz.go
+++ bincapz.go
@@ -6,13 +6,12 @@ package main
 
 import (
 	"context"
-	"flag"
-	"fmt"
 	"io/fs"
 	"log/slog"
 	"os"
 	"runtime"
 	"strings"
+	"time"
 
 	"github.com/chainguard-dev/bincapz/pkg/action"
 	"github.com/chainguard-dev/bincapz/pkg/bincapz"
@@ -23,6 +22,9 @@ import (
 	"github.com/chainguard-dev/bincapz/rules"
 	thirdparty "github.com/chainguard-dev/bincapz/third_party"
 	"github.com/chainguard-dev/clog"
+	"github.com/hillu/go-yara/v4"
+
+	"github.com/urfave/cli/v2"
 )
 
 var (
@@ -34,6 +36,29 @@ var (
 	ExitRenderFailed    = 11
 	ExitInvalidRules    = 14
 	ExitInvalidArgument = 22
+)
+
+var (
+	allFlag                   bool
+	concurrencyFlag           int
+	errFirstHitFlag           bool
+	errFirstMissFlag          bool
+	formatFlag                string
+	ignoreSelfFlag            bool
+	ignoreTagsFlag            string
+	includeDataFilesFlag      bool
+	minFileLevelFlag          int
+	minFileRiskFlag           string
+	minLevelFlag              int
+	minRiskFlag               string
+	ociFlag                   bool
+	outputFlag                string
+	profileFlag               bool
+	quantityIncreasesRiskFlag bool
+	statsFlag                 bool
+	thirdPartyFlag            bool
+	verboseFlag               bool
+	versionFlag               bool
 )
 
 // parse risk levels.
@@ -56,31 +81,6 @@ func parseRisk(s string) int {
 }
 
 func main() {
-	allFlag := flag.Bool("all", false, "Ignore nothing, show all")
-	concurrencyFlag := flag.Int("j", runtime.NumCPU(), "Concurrently scan files within target directories")
-	diffFlag := flag.Bool("diff", false, "Show capability drift between two files")
-	formatFlag := flag.String("format", "terminal", "Output type -- valid values are: json, markdown, simple, terminal, yaml")
-	ignoreSelfFlag := flag.Bool("ignore-self", true, "Ignore the bincapz binary")
-	ignoreTagsFlag := flag.String("ignore-tags", "", "Rule tags to ignore")
-	outputFlag := flag.String("o", "", "write output to this path instead of stdout")
-	includeDataFilesFlag := flag.Bool("data-files", false, "Include files that are detected as non-program (binary or source) files")
-	minFileLevelFlag := flag.Int("min-file-level", -1, "Obsoleted by --min-file-risk")
-	minLevelFlag := flag.Int("min-level", -1, "Obsoleted by --min-risk")
-	minFileRiskFlag := flag.String("min-file-risk", "low", "Only show results for files that meet this risk level (any,low,medium,high,critical")
-	minRiskFlag := flag.String("min-risk", "low", "Minimum risk level to show results for (any,low,medium,high,critical)")
-	errFirstMissFlag := flag.Bool("err-first-miss", false, "exit with error if scan source has no matching capabilities")
-	errFirstHitFlag := flag.Bool("err-first-hit", false, "exit with error if scan source has matching capabilities")
-	ociFlag := flag.Bool("oci", false, "Scan an OCI image")
-	quantityIncreasesRiskFlag := flag.Bool("quantity-increases-risk", true, "increase file risk score based on behavior quantity")
-	profileFlag := flag.Bool("profile", false, "Generate profile and trace files")
-	statsFlag := flag.Bool("stats", false, "Show statistics about the scan")
-	thirdPartyFlag := flag.Bool("third-party", true, "Include third-party rules, which may have licensing restrictions")
-	verboseFlag := flag.Bool("verbose", false, "Emit verbose logging messages to stderr")
-	versionFlag := flag.Bool("version", false, "Show version information")
-
-	flag.Parse()
-	args := flag.Args()
-
 	returnCode := ExitOK
 	defer func() { os.Exit(returnCode) }()
 
@@ -89,135 +89,300 @@ func main() {
 	logOpts := &slog.HandlerOptions{Level: logLevel, AddSource: true}
 	log := clog.New(slog.NewTextHandler(os.Stderr, logOpts))
 
-	var stop func()
-	if *profileFlag {
-		var err error
-		stop, err = profile.Profile()
-		if err != nil {
-			log.Error("profiling failed", slog.Any("error", err))
-			returnCode = ExitProfilerError
-			return
-		}
-	}
+	// variables to share between stages
+	var (
+		bc       bincapz.Config
+		ctx      context.Context
+		err      error
+		outFile  *os.File = os.Stdout
+		renderer bincapz.Renderer
+		res      *bincapz.Report
+		stop     func()
+		ver      string
+		yrs      *yara.Rules
+	)
 
-	if len(args) == 0 && !*versionFlag {
-		fmt.Printf("usage: bincapz [flags] <directories>")
-		returnCode = ExitInvalidArgument
-		return
-	}
-
-	if *verboseFlag {
-		logOpts.AddSource = true
-		logLevel.Set(slog.LevelDebug)
-	}
-
-	if *versionFlag {
-		ver, err := version.Version()
-		if err != nil {
-			fmt.Printf("bincapz unknown version\n")
-		}
-		fmt.Printf("%s\n", ver)
-		return
-	}
-
-	ctx := clog.WithLogger(context.Background(), log)
-	clog.FromContext(ctx).Info("bincapz starting")
-
-	ignoreTags := strings.Split(*ignoreTagsFlag, ",")
-	includeDataFiles := *includeDataFilesFlag
-	minRisk := parseRisk(*minRiskFlag)
-
-	// Backwards compatibility
-	if *minLevelFlag != -1 {
-		minRisk = *minLevelFlag
-	}
-
-	minFileRisk := parseRisk(*minFileRiskFlag)
-
-	// Backwards compatibility
-	if *minFileLevelFlag != -1 {
-		minFileRisk = *minFileLevelFlag
-	}
-
-	stats := *statsFlag
-	if *allFlag {
-		ignoreTags = []string{}
-		minRisk = -1
-		includeDataFiles = true
-		*ignoreSelfFlag = false
-	}
-
-	outFile := os.Stdout
-	var err error
-	if *outputFlag != "" {
-		outFile, err = os.OpenFile(*outputFlag, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0o600)
-		if err != nil {
-			log.Error("open file", slog.Any("error", err), slog.String("path", *outputFlag))
-			returnCode = ExitInputOutput
-			return
-		}
-	}
-	defer func() {
-		outFile.Close()
-	}()
-
-	renderer, err := render.New(*formatFlag, outFile)
-	if err != nil {
-		log.Error("invalid format", slog.Any("error", err), slog.String("format", *formatFlag))
-		returnCode = ExitInvalidArgument
-		return
-	}
-
-	rfs := []fs.FS{rules.FS}
-	if *thirdPartyFlag {
-		rfs = append(rfs, thirdparty.FS)
-	}
-
-	yrs, err := compile.Recursive(ctx, rfs)
-	if err != nil {
-		log.Error("YARA rule compilation", slog.Any("error", err))
-		returnCode = ExitInvalidRules
-		return
-	}
-
-	bc := bincapz.Config{
-		Concurrency:           *concurrencyFlag,
-		ErrFirstHit:           *errFirstHitFlag,
-		ErrFirstMiss:          *errFirstMissFlag,
-		IgnoreSelf:            *ignoreSelfFlag,
-		IgnoreTags:            ignoreTags,
-		IncludeDataFiles:      includeDataFiles,
-		MinFileRisk:           minFileRisk,
-		MinRisk:               minRisk,
-		OCI:                   *ociFlag,
-		QuantityIncreasesRisk: *quantityIncreasesRiskFlag,
-		Renderer:              renderer,
-		Rules:                 yrs,
-		ScanPaths:             args,
-		Stats:                 stats,
-	}
-
-	var res *bincapz.Report
-
-	if *diffFlag {
-		res, err = action.Diff(ctx, bc)
-	} else {
-		res, err = action.Scan(ctx, bc)
-	}
+	ver, err = version.Version()
 	if err != nil {
 		returnCode = ExitActionFailed
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-		return
 	}
 
-	err = renderer.Full(ctx, res)
-	if err != nil {
-		returnCode = ExitRenderFailed
-		log.Error("render failed", slog.Any("error", err))
-		return
+	app := &cli.App{
+		Name:      "bincapz",
+		Version:   ver,
+		Usage:     "Detect malicious program behaviors",
+		UsageText: "bincapz <flags> [diff, scan] <path>",
+		Compiled:  time.Now(),
+		After: func(c *cli.Context) error {
+			// Close our output file (or stdout) after commands have run
+			defer func() {
+				outFile.Close()
+			}()
+
+			// Stop profiling if command was executed with that flag
+			if profileFlag {
+				stop()
+			}
+			return nil
+		},
+		// Handled shared initialization (flag parsing, rule compilation, configuration)
+		Before: func(c *cli.Context) error {
+			if profileFlag {
+				var err error
+				stop, err = profile.Profile()
+				if err != nil {
+					log.Error("profiling failed", slog.Any("error", err))
+					returnCode = ExitProfilerError
+					return nil
+				}
+			}
+
+			if verboseFlag {
+				logOpts.AddSource = true
+				logLevel.Set(slog.LevelDebug)
+			}
+
+			ctx = clog.WithLogger(c.Context, log)
+			clog.FromContext(ctx).Info("bincapz starting")
+
+			ignoreTags := strings.Split(ignoreTagsFlag, ",")
+			includeDataFiles := includeDataFilesFlag
+			minRisk := parseRisk(minRiskFlag)
+
+			// Backwards compatibility
+			if minLevelFlag != -1 {
+				minRisk = minLevelFlag
+			}
+
+			minFileRisk := parseRisk(minFileRiskFlag)
+
+			// Backwards compatibility
+			if minFileLevelFlag != -1 {
+				minFileRisk = minFileLevelFlag
+			}
+
+			stats := statsFlag
+			if allFlag {
+				ignoreTags = []string{}
+				minRisk = -1
+				includeDataFiles = true
+				ignoreSelfFlag = false
+			}
+
+			if outputFlag != "" {
+				outFile, err = os.OpenFile(outputFlag, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0o600)
+				if err != nil {
+					log.Error("open file", slog.Any("error", err), slog.String("path", outputFlag))
+					returnCode = ExitInputOutput
+					return nil
+				}
+			}
+
+			renderer, err = render.New(formatFlag, outFile)
+			if err != nil {
+				log.Error("invalid format", slog.Any("error", err), slog.String("format", formatFlag))
+				returnCode = ExitInvalidArgument
+				return nil
+			}
+
+			rfs := []fs.FS{rules.FS}
+			if thirdPartyFlag {
+				rfs = append(rfs, thirdparty.FS)
+			}
+
+			yrs, err = compile.Recursive(ctx, rfs)
+			if err != nil {
+				log.Error("YARA rule compilation", slog.Any("error", err))
+				returnCode = ExitInvalidRules
+				return nil
+			}
+
+			bc = bincapz.Config{
+				Concurrency:           concurrencyFlag,
+				ErrFirstHit:           errFirstHitFlag,
+				ErrFirstMiss:          errFirstMissFlag,
+				IgnoreSelf:            ignoreSelfFlag,
+				IgnoreTags:            ignoreTags,
+				IncludeDataFiles:      includeDataFiles,
+				MinFileRisk:           minFileRisk,
+				MinRisk:               minRisk,
+				OCI:                   ociFlag,
+				QuantityIncreasesRisk: quantityIncreasesRiskFlag,
+				Renderer:              renderer,
+				Rules:                 yrs,
+				ScanPaths:             c.Args().Slice()[1:],
+				Stats:                 stats,
+			}
+
+			return nil
+		},
+		// Global flags shared between commands
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:        "all",
+				Value:       false,
+				Usage:       "Ignore nothing, show all",
+				Destination: &allFlag,
+			},
+			&cli.BoolFlag{
+				Name:        "err-first-miss",
+				Value:       false,
+				Usage:       "Exit with error if scan source has no matching capabilities",
+				Destination: &errFirstMissFlag,
+			},
+			&cli.BoolFlag{
+				Name:        "err-first-hit",
+				Value:       false,
+				Usage:       "Exit with error if scan source has matching capabilities",
+				Destination: &errFirstHitFlag,
+			},
+			&cli.StringFlag{
+				Name:        "format",
+				Value:       "terminal",
+				Usage:       "Output type -- valid values are: json, markdown, simple, terminal, yaml",
+				Destination: &formatFlag,
+			},
+			&cli.BoolFlag{
+				Name:        "ignore-self",
+				Value:       true,
+				Usage:       "Ignore the bincapz binary",
+				Destination: &ignoreSelfFlag,
+			},
+			&cli.StringFlag{
+				Name:        "ignore-tags",
+				Value:       "",
+				Usage:       "Rule tags to ignore",
+				Destination: &ignoreTagsFlag,
+			},
+			&cli.BoolFlag{
+				Name:        "include-data-files",
+				Value:       false,
+				Usage:       "Include files that are detected as non-program (binary or source) files",
+				Destination: &includeDataFilesFlag,
+			},
+			&cli.IntFlag{
+				Name:        "j",
+				Value:       runtime.NumCPU(),
+				Usage:       "Concurrently scan files within target directories",
+				Destination: &concurrencyFlag,
+			},
+			&cli.IntFlag{
+				Name:        "min-file-level",
+				Value:       -1,
+				Usage:       "Obsoleted by --min-file-risk",
+				Destination: &minFileLevelFlag,
+			},
+			&cli.StringFlag{
+				Name:        "min-file-risk",
+				Value:       "low",
+				Usage:       "Only show results for files that meet this risk level (any,low,medium,high,critical)",
+				Destination: &minFileRiskFlag,
+			},
+			&cli.IntFlag{
+				Name:        "min-level",
+				Value:       -1,
+				Usage:       "Obsoleted by --min-risk",
+				Destination: &minLevelFlag,
+			},
+			&cli.StringFlag{
+				Name:        "min-risk",
+				Value:       "low",
+				Usage:       "Minimum risk level to show results for (any,low,medium,high,critical)",
+				Destination: &minRiskFlag,
+			},
+			&cli.BoolFlag{
+				Name:        "oci",
+				Value:       false,
+				Usage:       "Scan an OCI image",
+				Destination: &ociFlag,
+			},
+			&cli.StringFlag{
+				Name:        "output",
+				Aliases:     []string{"o"},
+				Value:       "",
+				Usage:       "Write output to specified file instead of stdout",
+				Destination: &outputFlag,
+			},
+			&cli.BoolFlag{
+				Name:        "profile",
+				Aliases:     []string{"p"},
+				Value:       false,
+				Usage:       "Generate profile and trace files",
+				Destination: &profileFlag,
+			},
+			&cli.BoolFlag{
+				Name:        "quantity-increases-risk",
+				Value:       true,
+				Usage:       "Increase file risk score based on behavior quantity",
+				Destination: &quantityIncreasesRiskFlag,
+			},
+			&cli.BoolFlag{
+				Name:        "stats",
+				Aliases:     []string{"s"},
+				Value:       false,
+				Usage:       "Show scan statistics",
+				Destination: &statsFlag,
+			},
+			&cli.BoolFlag{
+				Name:        "third-party",
+				Value:       true,
+				Usage:       "Include third-party rules which may have licensing restrictions",
+				Destination: &thirdPartyFlag,
+			},
+			&cli.BoolFlag{
+				Name:        "verbose",
+				Value:       false,
+				Usage:       "Emit verbose logging messages to stderr",
+				Destination: &verboseFlag,
+			},
+		},
+		Commands: []*cli.Command{
+			{
+				Name:  "diff",
+				Usage: "scan and diff two paths",
+				Action: func(c *cli.Context) error {
+					res, err = action.Diff(ctx, bc)
+					if err != nil {
+						log.Error("diff failed", slog.Any("error", err))
+						returnCode = ExitActionFailed
+						return err
+					}
+
+					err = renderer.Full(ctx, res)
+					if err != nil {
+						log.Error("render failed", slog.Any("error", err))
+						returnCode = ExitRenderFailed
+						return err
+					}
+					return nil
+				},
+			},
+			{
+				Name:  "scan",
+				Usage: "scan a single path",
+				Action: func(c *cli.Context) error {
+					res, err = action.Scan(ctx, bc)
+					if err != nil {
+						log.Error("scan failed", slog.Any("error", err))
+						returnCode = ExitActionFailed
+						return err
+					}
+
+					err = renderer.Full(ctx, res)
+					if err != nil {
+						log.Error("render failed", slog.Any("error", err))
+						returnCode = ExitRenderFailed
+						return err
+					}
+
+					return nil
+				},
+			},
+		},
 	}
 
-	if *profileFlag {
-		stop()
+	if err := app.Run(os.Args); err != nil {
+		log.Error("error running bincapz: %w", slog.Any("error", err))
+		returnCode = ExitActionFailed
 	}
 }
