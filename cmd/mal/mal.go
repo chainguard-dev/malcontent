d0086d7a851f4e47a60d96a9b458bfefd163bfed
egibs <20933572+egibs@users.noreply.github.com>
2024-10-02 19:09:41 -0500

    Move comment
    Signed-off-by: egibs <20933572+egibs@users.noreply.github.com>

--- cmd/mal/mal.go
+++ cmd/mal/mal.go
@@ -213,12 +213,12 @@ func main() {
 				return err
 			}
 
+			// Add the default tags to ignore regardless of whether they're passed in or not
 			defaultIgnore := []string{
 				"false_positive",
 				"ignore",
 			}
 
-			// Add the default tags to ignore regardless of whether they're passed in or not
 			for _, t := range defaultIgnore {
 				if !slices.Contains(ignoreTags, t) {
 					ignoreTags = append(ignoreTags, t)
