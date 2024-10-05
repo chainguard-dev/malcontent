55cf384e2ecb63674b3ae548575b6120a7a0afe2
egibs <20933572+egibs@users.noreply.github.com>
2024-10-05 18:25:14 -0500

    Add rule and string match count, appease the linter
    Signed-off-by: egibs <20933572+egibs@users.noreply.github.com>

--- pkg/render/strings.go
+++ pkg/render/strings.go
@@ -56,9 +56,9 @@ func NewStringMatches(w io.Writer) StringMatches {
 
 type Match struct {
 	Description string
-	Matches     []string
 	Risk        int
 	Rule        string
+	Strings     []string
 }
 
 func (r StringMatches) File(_ context.Context, fr *malcontent.FileReport) error {
@@ -71,21 +71,23 @@ func (r StringMatches) File(_ context.Context, fr *malcontent.FileReport) error 
 		return fr.Behaviors[i].RuleName < fr.Behaviors[j].RuleName
 	})
 	for _, b := range fr.Behaviors {
-		if b.MatchStrings != nil {
+		if len(b.MatchStrings) > 0 {
 			matches = append(matches, Match{
-				Matches: b.MatchStrings,
 				Risk:    b.RiskScore,
 				Rule:    b.RuleName,
+				Strings: b.MatchStrings,
 			})
 		}
 	}
 
-	prefix := "String matches for"
-	header := fmt.Sprintf("%s %s %s%s%s:\n", prefix, color.HiGreenString(fr.Path), color.HiBlackString("["), briefRiskColor(fr.RiskLevel), color.HiBlackString("]"))
-	fmt.Fprintf(r.w, header)
+	prefix := "Matches for"
+	rUnit := plural("rule", len(matches))
+	fmt.Fprintf(r.w, "%s %s %s%s%s %s%s %s%s:\n", prefix, color.HiGreenString(fr.Path), color.HiBlackString("["), briefRiskColor(fr.RiskLevel), color.HiBlackString("]"), color.HiBlackString("("), color.HiGreenString(fmt.Sprintf("%d", len(matches))), color.HiGreenString(rUnit), color.HiBlackString(")"))
 	for _, m := range matches {
-		fmt.Fprintf(r.w, "%s %s%s%s: \n%s%s\n", color.HiGreenString(m.Rule), color.HiBlackString("["), briefRiskColor(riskLevels[m.Risk]), color.HiBlackString("]"), color.HiBlackString("- "), strings.Join(m.Matches, color.HiBlackString("\n- ")))
+		sUnit := plural("string", len(m.Strings))
+		fmt.Fprintf(r.w, "%s %s%s%s %s%s %s%s: \n%s%s\n", color.HiCyanString(m.Rule), color.HiBlackString("["), briefRiskColor(riskLevels[m.Risk]), color.HiBlackString("]"), color.HiBlackString("("), color.HiGreenString(fmt.Sprintf("%d", len(m.Strings))), color.HiGreenString(sUnit), color.HiBlackString(")"), color.HiBlackString("- "), strings.Join(m.Strings, color.HiBlackString("\n- ")))
 	}
+
 	return nil
 }
 
@@ -96,4 +98,12 @@ func (r StringMatches) Full(_ context.Context, rep *malcontent.Report) error {
 	}
 
 	return fmt.Errorf("diffs are unsupported by the StringMatches renderer")
+}
+
+// plural returns a pluralized string if the length of l is greater than 1.
+func plural(s string, l int) string {
+	if l > 1 {
+		return fmt.Sprintf("%ss", s)
+	}
+	return s
 }
