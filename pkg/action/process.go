ae069751f36dc92b123eb945899cfa9a1a540007
Thomas Stromberg <t+github@chainguard.dev>
2024-10-07 10:48:49 -0400

    processes: improve results on Linux

--- pkg/action/process.go
+++ pkg/action/process.go
@@ -12,57 +12,45 @@ import (
 	"github.com/shirou/gopsutil/v4/process"
 )
 
-type Process struct {
-	PID  int32
-	Path string
+type ProcessInfo struct {
+	PID            int32
+	PPID           int32
+	Name           string
+	ScanPath       string
+	AdvertisedPath string
+	CmdLine        []string
 }
 
-// ActiveProcessPaths is an exported function that returns a slice of Process PIDs and commands (path).
-func ActiveProcessPaths(ctx context.Context) ([]Process, error) {
+// ActiveProcesses is an exported function that a list of active processes
+func ActiveProcesses(ctx context.Context) ([]*ProcessInfo, error) {
 	// Retrieve all of the active PIDs
 	procs, err := process.ProcessesWithContext(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("processes: %w", err)
 	}
 
-	found := map[string]Process{}
+	found := map[string]*ProcessInfo{}
 	for _, p := range procs {
-		// Skip Linux kernel threads
-		if runtime.GOOS == "linux" {
-			if p.Pid == 2 {
-				continue
-			}
-			parent, _ := p.PpidWithContext(ctx)
-			if parent == 2 {
-				continue
-			}
-		}
-
-		path, err := processPath(ctx, p)
+		pi, err := processInfo(ctx, p)
 		if err != nil {
 			clog.Warnf("skipping pid %d: %v", p.Pid, err)
 			continue
 		}
-
-		// NOTE: this only stores the last pid we found for a process, we may want to adjust that
-		found[path] = Process{
-			PID:  p.Pid,
-			Path: path,
+		if pi == nil {
+			continue
 		}
+
+		found[pi.ScanPath] = pi
 	}
 
-	ps := make([]Process, 0, len(found))
+	ps := make([]*ProcessInfo, 0, len(found))
 	for _, v := range found {
 		ps = append(ps, v)
 	}
 
 	sort.Slice(ps, func(i, j int) bool {
-		return ps[i].Path < ps[j].Path
+		return ps[i].ScanPath < ps[j].ScanPath
 	})
-
-	for _, p := range ps {
-		clog.Infof("%d=%d", p.Path, p.PID)
-	}
 
 	return ps, nil
 }
@@ -73,37 +61,64 @@ func canStat(path string) bool {
 	return err == nil
 }
 
-// processPath returns the best path to a process executable
-func processPath(ctx context.Context, p *process.Process) (string, error) {
-	// on Linux, this is effectively readlink(/proc/X/exe)
-	path, eErr := p.Exe()
-	if eErr == nil {
-		if canStat(path) {
-			return path, nil
-		}
-		// maybe the path was deleted? fallback to the original executable
-		if runtime.GOOS == "linux" {
-			path := fmt.Sprintf("/proc/%d/exe", p.Pid)
-			if canStat(path) {
-				return path, nil
-			}
-		}
+// processInfo returns the best path to a process executable
+func processInfo(ctx context.Context, p *process.Process) (*ProcessInfo, error) {
+	pi := &ProcessInfo{
+		PID: p.Pid,
 	}
-
-	// on Linux, p.Exe() isn't available for other users processes unless you are root, try an alternate route.
-	cmd, err := p.CmdlineSliceWithContext(ctx)
-	if err == nil {
-		if len(cmd) > 0 && strings.HasPrefix(cmd[0], "/") {
-			if canStat(cmd[0]) {
-				return cmd[0], nil
-			}
-			return "", fmt.Errorf("%q is inacessible", cmd[0])
-		}
-	}
-
 	name, err := p.Name()
 	if err != nil {
 		name = "<unknown>"
 	}
-	return "", fmt.Errorf("%q: %w", name, eErr)
+	pi.Name = name
+
+	parent, err := p.PpidWithContext(ctx)
+	if err != nil {
+		parent = -1
+	}
+
+	// Skip Linux kernel threads that have no backing executable
+	if runtime.GOOS == "linux" && (p.Pid == 2 || parent == 2) {
+		return nil, nil
+	}
+	pi.PPID = parent
+
+	cmd, err := p.CmdlineSliceWithContext(ctx)
+	if err == nil {
+		pi.CmdLine = cmd
+		if len(cmd) > 0 && strings.HasPrefix(cmd[0], "/") {
+			pi.AdvertisedPath = cmd[0]
+		}
+	}
+
+	// on Linux, this is effectively readlink(/proc/X/exe), but it isn't fully resolved either
+	path, err := p.Exe()
+	if err == nil {
+		pi.ScanPath = path
+		if canStat(pi.ScanPath) {
+			return pi, nil
+		}
+	}
+
+	// fallback if p.Exe fails to be stattable
+	if runtime.GOOS == "linux" {
+		pi.ScanPath = fmt.Sprintf("/proc/%d/exe", p.Pid)
+
+		if canStat(pi.ScanPath) {
+			return pi, nil
+		}
+	}
+
+	// Settle for whatever binary we may have found in the process table
+	if canStat(pi.AdvertisedPath) {
+		pi.ScanPath = pi.AdvertisedPath
+		return pi, nil
+	}
+
+	if pi.AdvertisedPath != "" {
+		return nil, fmt.Errorf("%s[%d]: unable to stat %q or %q", pi.Name, pi.PID, pi.ScanPath, pi.AdvertisedPath)
+	}
+
+	return nil, fmt.Errorf("%s: unable to stat %q", pi.Name, pi.ScanPath)
+
 }
