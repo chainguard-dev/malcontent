87ecd6638092f8196087b8a338d2b3192fcb3290
egibs <20933572+egibs@users.noreply.github.com>
2024-12-30 12:29:14 -0600

    Run make fix
    Signed-off-by: egibs <20933572+egibs@users.noreply.github.com>

--- pkg/render/terminal.go
+++ pkg/render/terminal.go
@@ -291,10 +291,10 @@ func renderFileSummary(_ context.Context, fr *malcontent.FileReport, w io.Writer
 				diff = color.HiGreenString("+")
 			}
 
-			if riskScore > previousNsRiskScore[ns]{
+			if riskScore > previousNsRiskScore[ns] {
 				nsIcon = color.HiYellowString("▲")
 			}
-			if riskScore < previousNsRiskScore[ns]{
+			if riskScore < previousNsRiskScore[ns] {
 				nsIcon = color.HiGreenString("▼")
 			}
 			if riskLevel == "NONE" {
