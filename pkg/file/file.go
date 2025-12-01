bc0121281c86d2e535ab9c5a7eaac40186ec49f8
egibs <20933572+egibs@users.noreply.github.com>
2025-12-01 11:55:26 -0600

    fix comment
    Signed-off-by: egibs <20933572+egibs@users.noreply.github.com>

--- pkg/file/file.go
+++ pkg/file/file.go
@@ -5,7 +5,7 @@ const (
 	DefaultPoolBuffer int64 = 4 * 1024   // 4KB
 	ExtractBuffer     int64 = 64 * 1024  // 64KB
 	MaxPoolBuffer     int64 = 128 * 1024 // 128KB
-	MaxBytes          int64 = 1 << 32    // 2048MB
+	MaxBytes          int64 = 1 << 32    // 4096MB
 	ReadBuffer        int64 = 64 * 1024  // 64KB
 	ZipBuffer         int64 = 2 * 1024   // 2KB
 )
