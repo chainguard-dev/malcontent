d6ec1cd907d142e59fb4215ae8c485dcef394c83
egibs <20933572+egibs@users.noreply.github.com>
2025-12-01 07:57:01 -0600

    move the pool values as well
    Signed-off-by: egibs <20933572+egibs@users.noreply.github.com>

--- pkg/file/file.go
+++ pkg/file/file.go
@@ -2,8 +2,10 @@ package file
 
 // common values used across malcontent for extracting and reading files.
 const (
-	ExtractBuffer       = 64 * 1024 // 64KB
-	MaxBytes            = 1 << 32   // 2048MB
-	ReadBuffer    int64 = 64 * 1024 // 64KB
-	ZipBuffer           = 2 * 1024  // 2KB
+	DefaultPoolBuffer int   = 4 * 1024   // 4KB
+	ExtractBuffer           = 64 * 1024  // 64KB
+	MaxPoolBuffer     int   = 128 * 1024 // 128KB
+	MaxBytes                = 1 << 32    // 2048MB
+	ReadBuffer        int64 = 64 * 1024  // 64KB
+	ZipBuffer               = 2 * 1024   // 2KB
 )
